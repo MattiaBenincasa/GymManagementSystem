389e0d1dcf6bf07ad47f878b40da17e2a794d6ab
MattiaBenincasa <mattia.benincasa03@gmail.com>
2025-07-31 20:40:21 +0200

    Added two new discount strategies

---
+++ src/main/java/DomainModel/PurchaseItems/PurchaseCalculator/SeasonalDiscount.java
@@ -0,0 +1,52 @@
+package DomainModel.PurchaseItems.PurchaseCalculator;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashSet;
+
+public class SeasonalDiscount implements DiscountStrategy{
+    private String description;
+    private final HashSet<Month> monthsWithDiscount;
+    private float percentage;
+
+    public SeasonalDiscount(float percentage) {
+        this.percentage = percentage;
+        this.monthsWithDiscount = new HashSet<>();
+    }
+
+    @Override
+    public float applyDiscount(float price) {
+        float discountedPrice = price;
+        if (monthsWithDiscount.contains(LocalDate.now().getMonth()))
+            discountedPrice = this.percentageDiscount(discountedPrice, this.percentage);
+        return discountedPrice;
+    }
+
+    public void addMonth(Month month) {
+        this.monthsWithDiscount.add(month);
+    }
+
+    public void removeMonth(Month month) {
+        this.monthsWithDiscount.remove(month);
+    }
+
+    public HashSet<Month> getMonth() {
+        return this.monthsWithDiscount;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPercentage(float percentage) {
+        this.percentage = percentage;
+    }
+}
