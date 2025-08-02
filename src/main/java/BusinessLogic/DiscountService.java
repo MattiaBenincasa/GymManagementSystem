2dabf7abb3a94c65f5d532480e969b2615443716
MattiaBenincasa <mattia.benincasa03@gmail.com>
2025-08-02 19:33:03 +0200

    Added the skeleton of the main discount services

--- src/main/java/BusinessLogic/DiscountService.java
+++ src/main/java/BusinessLogic/DiscountService.java
@@ -1,23 +1,35 @@
 package BusinessLogic;
 
 import DomainModel.DiscountStrategy.DiscountStrategy;
-import DomainModel.Membership.Membership;
+import DomainModel.DiscountStrategy.PercentageDiscount;
+import DomainModel.Membership.Purchasable;
 
 public class DiscountService {
     //TODO add discount DAO
 
-    public void createDiscount() {}
+    public void createPercentageDiscount(int percentage, boolean isSpecialOffer, String description) {
+        PercentageDiscount percentageDiscount = new PercentageDiscount(percentage, isSpecialOffer, description);
+        // TODO pass this object to a DAO, it will store it in db and return a id
+        // FIXME this function should return an integer, the id
+    }
 
-    public void deleteDiscount() {}
+    public void deleteDiscount() {
 
-    public void addDiscountToMembership(Membership membership, DiscountStrategy discountStrategy) {
-        membership.addDiscount(discountStrategy);
+    }
+
+    // TODO adding all updating method. DAO will contain only one method update
+    // TODO that will update all attributes of the object
+
+    public void addDiscountToPurchasable(Purchasable purchasable, DiscountStrategy discountStrategy) {
+        purchasable.addDiscount(discountStrategy);
         // TODO Save into DB
     }
 
-    public void addDiscountToBundle(Membership membership, DiscountStrategy discountStrategy) {
-        membership.addDiscount(discountStrategy);
+    public void removeDiscountToPurchasable(Purchasable purchasable, DiscountStrategy discountStrategy) {
+        purchasable.removeDiscount(discountStrategy);
         // TODO Save into DB
     }
+
+    public void addSameDiscountToMultiplePurchasable() {}
 
 }
--- src/main/java/BusinessLogic/Validators/BasicValidator.java
+++ src/main/java/BusinessLogic/Validators/BasicValidator.java
@@ -6,4 +6,10 @@ public class BasicValidator {
 
     public void checkMedicalCertificate(Customer customer) {}
 
+    public void checkRegistrationFee(Customer customer) {
+        // get all registration fee from db with DAO
+        // and check if one of them is valid. A registrationFee
+        // can be invalid if is expired or is not activated yet
+    }
+
 }
